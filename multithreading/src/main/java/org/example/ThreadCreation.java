c0df47938c77207ab04a1747c696b5e0a65f06cc
Mkknowledge <kmayur2124@gmail.com>
2024-03-24 02:43:22 +0530

    Thread Creation Leacture Part 1

--- multithreading/src/main/java/org/example/ThreadCreation.java
+++ multithreading/src/main/java/org/example/ThreadCreation.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class ThreadCreation {
     public static void main(String[] args) throws InterruptedException {
+
+        /*
+        * In Java, all the threads related properties and methods are encapsulated in the thread class by the JDK.
+        * So in order for us to create a new thread, we first need to create a new threat object.
+        * The threat object itself is empty by default.
+        * So we need to pass it an object of a class that implements the runnable interface into its constructor.
+        *  */
+
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -13,10 +21,22 @@ public class ThreadCreation {
         thread.setName("New Worker Thread");
         thread.setPriority(Thread.MAX_PRIORITY);
 
+
         System.out.println("we are in thread: " + Thread.currentThread().getName() + " before starting the new thread.");
+
+        /*
+        * We need to actually start the thread by calling the start method on the thread object.
+        * This will instruct the JVM to create a new thread and pass it to the operating system.
+        * */
         thread.start();
         System.out.println("we are in thread: " + Thread.currentThread().getName() + " after starting the new thread.");
 
-        //Thread.sleep(10000);
+        /*
+        * The threat class also has a static method called sleep, which puts the current threat to sleep for a given number of milliseconds.
+        * It's important to point out that this does not spin in a loop or anything like that.
+        * Instead, the threat sleep method, instructs the operating system to not schedule the current thread until that time passes.
+        * During that time, this thread is not consuming any CPU.
+        * */
+        Thread.sleep(10000);
     }
 }
--- multithreading/src/main/java/org/example/ThreadUnCaughtException.java
+++ multithreading/src/main/java/org/example/ThreadUnCaughtException.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class ThreadUnCaughtException {
     public static void main(String[] args) throws InterruptedException {
+
+        /*
+        * Normally unchecked exceptions that happen in Java, simply bring down the entire thread unless we catch them explicitly and handle them in a particular way.
+        * With thread.setUncaughtExceptionHandler(), We can set an exception handler for the entire thread at its inception.
+        * That handler will be called if an exception was thrown inside the thread and did not get caught anywhere.
+        * */
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
